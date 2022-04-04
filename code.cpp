7c55fbc045de85cee4b40d1c52c315e951c19d49
QQ-no-newbie <1770362456@qq.com>
2022-04-04 23:27:58 +0800

    main

--- code.cpp
+++ code.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define endl '\n'
 #define u_set unordered_set
-#define u_map unordered_map 
+#define u_map unordered_map
 #define vi vector<int>
 #define pll pair<long long, long long>
 #define vll vector<long long>
@@ -24,6 +24,7 @@ int main() {
 }
 
 void solve() {
-  
+  cin >> n >> m;
+  cout << n << " " << m << endl;
   return;
 }
